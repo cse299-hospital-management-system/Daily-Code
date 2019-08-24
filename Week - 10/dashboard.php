1a876f9777072f27d502548fae46d75f199d2b3d
Seragis Salekin <52543026+SeragisSalekin@users.noreply.github.com>
2019-08-24 08:31:54 +0600

    Add files via upload

---
+++ Week - 10/dashboard.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+//error_reporting(0);
+include('include/config.php');
+include('include/checklogin.php');
+check_login();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Doctor</title>
+
+
+	</head>
+	<body>
+		<div id="app">		
+<?php include('include/sidebar.php');?>
+			<div class="app-content">
+				
+						<?php include('include/header.php');?>
+						
+				<!-- end: TOP NAVBAR -->
+				<div class="main-content" >
+					<div class="wrap-content container" id="container">
+						<!-- start: PAGE TITLE -->
+						<section id="page-title">
+							<div class="row">
+								<div class="col-sm-8">
+									<h1 class="mainTitle">Doctor</h1>
+																	</div>
+								<ol class="breadcrumb">
+									<li>
+										<span>User</span>
+									</li>
+								</ol>
+							</div>
+						</section>
+						<!-- end: PAGE TITLE -->
+						<!-- start: BASIC EXAMPLE -->
+							<div class="container-fluid container-fullw bg-white">
+							<div class="row">
+								<div class="col-sm-4">
+									<div class="panel panel-white no-radius text-center">
+										<div class="panel-body">
+											<span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-smile-o fa-stack-1x fa-inverse"></i> </span>
+											<h2 class="StepTitle">My Profile</h2>
+											
+											<p class="links cl-effect-1">
+												<a href="edit-profile.php">
+													Update Profile
+												</a>
+											</p>
+										</div>
+									</div>
+								</div>
+			
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
